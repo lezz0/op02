fbb9ab22826f33b9250042a06b41af455730e9fc
Lorenz Nunis <lorenz@Lorenzs-MacBook-Pro-2.local>
2025-12-08 23:20:02 +0800

    Fix checkout page for Vercel deploy

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -4,6 +4,9 @@ import { useSearchParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
 
+export const dynamic = "force-dynamic";
+
+
 export default function CheckoutPage() {
   const searchParams = useSearchParams();
   const router = useRouter();
